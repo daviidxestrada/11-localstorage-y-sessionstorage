855688b2a7b4d45ee0341b67d36f71df7719d196
unknown <daviidxestrada@gmail.com>
2025-10-08 18:10:42 +0200

    practicaChatty

--- js/main.js
+++ js/main.js
@@ -15,3 +15,18 @@ const recuperarAlumno = ()=>{
 
 guardarAlumno()
 recuperarAlumno()
+
+// PRUEBA
+
+
+
+    const cursos = ["HTML","CSS","JS"]
+localStorage.setItem("cursos",JSON.stringify(cursos))
+
+
+const cursosRecuperados = JSON.parse(localStorage.getItem("cursos"))
+console.log(cursosRecuperados)
+
+
+guardarCursos()
+recuperarCursos()
